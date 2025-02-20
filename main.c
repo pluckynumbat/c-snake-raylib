eba5b4bf7de426fb668c3db003b34217d5bb8891
Neehar Patil <pluckynumbat@gmail.com>
2025-02-20 11:56:17 +0530

    added some constants for the snake dimensions and color

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
 #include "raylib.h"
 
+////Constants////
+
+const int SNAKE_CELL_RADIUS = 4;
+const int DEFAULT_SNAKE_CELL_DIFF = 4;
+
+const int STARTING_SNAKE_LENGTH = 10;
+
+const Color SNAKE_COLOR = {
+    .r = 240,
+    .g = 140,
+    .b = 0,
+    .a = 255,
+};
+
+///end Constants////
+
 int main()
 {
     InitWindow(200, 200, "C-Snake");
