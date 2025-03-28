039583305cff20309cfb42a8e80fe73dd013b1c3
Neehar Patil <pluckynumbat@gmail.com>
2025-03-28 15:32:20 +0530

    comments for the constants in the header file

--- csnake.h
+++ csnake.h
@@ -1,9 +1,14 @@
 #ifndef C_SNAKE_H
 #define C_SNAKE_H
 
+////Constants////
+
+// Related to the window
 const int WINDOW_WIDTH = 200;
 const int WINDOW_HEIGHT = 200;
 const char* GAME_TITLE = "C-Snake";
+
+
 
 
 #endif
